c993dbecf979f93d66a77756bc4bf2d0564bf5ca
Chloe Verity <chloeverity@hotmail.co.uk>
2018-12-11 14:33:15 +0000

    adding mobile number and name to user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user.as_json(auth_token: user.authentication_token, email: user.email), status: 201
+      render json: user.as_json(auth_token: user.authentication_token, email: user.email, name: user.name, mobile: user.mobile), status: 201
       return
     else
       warden.custom_failure!
