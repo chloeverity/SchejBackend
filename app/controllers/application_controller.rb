b1be9c839a2cb730e5dbc516589ea52723440374
Chloe Verity <chloeverity@hotmail.co.uk>
2018-12-11 11:27:07 +0000

    working on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  # def protect_from_forgery
+  #
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #defaults: { format: :json }
   namespace :api do
     namespace :v1 do
       resources :problems
