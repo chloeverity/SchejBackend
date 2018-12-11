560aa7220958e3c69816cd5381913d98554791d4
Becka <rebecca.lelew@gmail.com>
2018-12-11 15:54:32 +0000

    TDDs shift model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :shifts, dependent: :destroy
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
---
+++ spec/models/shift_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Shift, type: :model do
+
+  it 'belongs to a user' do
+    association = described_class.reflect_on_association(:user).macro
+    expect(association).to eq :belongs_to
+  end
+end
