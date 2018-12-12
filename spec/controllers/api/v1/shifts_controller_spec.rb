07c208918d989bed6138f287125b831c312c0029
Becka <rebecca.lelew@gmail.com>
2018-12-12 15:35:40 +0000

    Changes start and end time to integer

--- spec/controllers/api/v1/shifts_controller_spec.rb
+++ spec/controllers/api/v1/shifts_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Api::V1::ShiftsController, type: :request do
 
   describe 'creating a shift' do
     it 'creates a shift with title, start time and end time' do
-      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => DateTime.now, 'end_time' => DateTime.now, 'user_id' => 1}
+      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => 1517540400000, 'end_time' => 1517540400000, 'user_id' => 1}
       expect(JSON.parse(response.body)).to include "title" => "test@test.com"
     end
   end
@@ -19,15 +19,15 @@ RSpec.describe Api::V1::ShiftsController, type: :request do
 
     it 'shows all shifts' do
       post '/api/v1/sign_in', :params => {'email' => 'test@test.com', 'password' => 'testpassword'}
-      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => DateTime.now, 'end_time' => DateTime.now, 'user_id' => 1}
+      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => 1517540400000, 'end_time' => 1517540400000, 'user_id' => 1}
       get '/api/v1/shifts'
       expect(JSON.parse(response.body).first).to include('title' => 'test@test.com')
     end
   end
   describe 'deleting a shift' do
     it 'deletes a shift and its information' do
-      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => DateTime.now, 'end_time' => DateTime.now, 'user_id' => 1}
-      post '/api/v1/shifts', :params => {'title' => 'test1@test.com', 'start_time' => DateTime.now, 'end_time' => DateTime.now, 'user_id' => 1}
+      post '/api/v1/shifts', :params => {'title' => 'test@test.com', 'start_time' => 1517540400000, 'end_time' => 1517540400000, 'user_id' => 1}
+      post '/api/v1/shifts', :params => {'title' => 'test1@test.com', 'start_time' => 1517540400000, 'end_time' => 1517540400000, 'user_id' => 1}
       id = 1
       delete "/api/v1/shifts/#{id}"
       get '/api/v1/shifts'
