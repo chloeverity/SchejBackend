ac7a2a3f079ac0a34e3cd3a0b87e3b225fdbe754
Becka <rebecca.lelew@gmail.com>
2018-12-12 10:13:43 +0000

    Committing before pulling from master

---
+++ app/controllers/api/v1/shifts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::ShiftsController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ActionController::ImplicitRender
+
+  def create
+  end
+
+
+end
