77d4938ff7b17c338aa7a95026fe926ffddcae51
Becka <rebecca.lelew@gmail.com>
2018-12-18 11:41:44 +0000

    Shows emergency requests if user has same org and job title as requester

--- app/controllers/api/v1/emergency_requests_controller.rb
+++ app/controllers/api/v1/emergency_requests_controller.rb
@@ -4,10 +4,28 @@ class Api::V1::EmergencyRequestsController < ApplicationController
     emergency_requester_id = Shift.find(params[:emergency_shift_id]).user_id
     @emergency_request = EmergencyRequest.new(request_params.merge(emergency_requester_id: emergency_requester_id))
     if @emergency_request.save!
-      render json: format_json_for_request(@emergency_request), status: :created
+      render json: format_json(@emergency_request), status: :created
     else
       head(:unprocessable_entity)
     end
+  end
+
+  def index
+    organisation = User.find(params[:user_id]).organisation
+    job_title = User.find(params[:user_id]).job_title
+
+    @emergency_requests = EmergencyRequest.all
+
+    relevant_requests = []
+
+    @emergency_requests.each do |request|
+      requester = User.find(request.emergency_requester_id)
+      requester_organisation = requester.organisation
+      requester_job_title = requester.job_title
+      relevant_requests << request if (requester_organisation == organisation && requester_job_title == job_title)
+    end
+
+    render json: relevant_requests.map { |request| format_json(request) }
   end
 
   private
@@ -16,12 +34,12 @@ class Api::V1::EmergencyRequestsController < ApplicationController
       params.permit(:comment, :emergency_shift_id)
   end
 
-  def format_json_for_request(emergency_request)
+  def format_json(emergency_request)
     requested_emergency_shift = Shift.find(emergency_request.emergency_shift_id)
     emergency_requester_id = User.find(emergency_request.emergency_requester_id)
 
     {id: emergency_request.id, comment: emergency_request.comment,
-        id: emergency_request.emergency_shift_id,
+        shiftId: emergency_request.emergency_shift_id,
         userId: emergency_request.emergency_requester_id,
         name: emergency_requester_id.name,
         start: requested_emergency_shift.start_time,
--- spec/controllers/api/v1/emergency_requests_controller_spec.rb
+++ spec/controllers/api/v1/emergency_requests_controller_spec.rb
@@ -8,10 +8,13 @@ RSpec.describe Api::V1::EmergencyRequestsController, type: :request do
   let(:job_title) {'coach'}
   let(:user_2_email) { 'test2@test.com' }
   let(:user_2_name) { 'user2' }
+  let(:user_3) { {email: 'test3@test.com', name: 'user3', job_title: 'manager' } }
 
   before(:each) do
     @user_1_id = sign_up_get_user_id(user_1_email, org, user_1_name, job_title)
     @shift_1_id = post_shift_get_id(@user_1_id)
+    @user_2_id = sign_up_get_user_id(user_2_email, org, user_2_name, job_title)
+    @user_3_id = sign_up_get_user_id(user_3[:email], org, user_3[:name], user_3[:job_title])
   end
 
   describe 'new emergency request' do
@@ -19,6 +22,18 @@ RSpec.describe Api::V1::EmergencyRequestsController, type: :request do
       post '/api/v1/emergency_requests', params: { 'emergency_shift_id' => @shift_1_id, 'comment' => 'hello' }
       expect(JSON.parse(response.body)).to include "userId" => @user_1_id
     end
-  end
 
+    it 'shows all emergency notifications for the same organisation and job title' do
+      post '/api/v1/emergency_requests', params: { 'emergency_shift_id' => @shift_1_id, 'comment' => 'hello' }
+      get '/api/v1/emergency_requests', params: { 'user_id' => @user_2_id }
+      expect(JSON.parse(response.body).first).to include "userId" => @user_1_id
+      expect(JSON.parse(response.body).first).to include "shiftId" => @shift_1_id
+    end
+
+    it 'does not show emergency request if user has different job title to requester' do
+      post '/api/v1/emergency_requests', params: { 'emergency_shift_id' => @shift_1_id, 'comment' => 'hello' }
+      get '/api/v1/emergency_requests', params: { 'user_id' => @user_3_id }
+      expect(JSON.parse(response.body).length).to eq 0
+    end
+  end
 end
